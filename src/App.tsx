9a98c5b875c25ee6bdfbcc03ab87a1235bbcb6dd
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-21 23:31:52 -0300

    Introducción a CSS Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-
+import styles from "./App.module.css"
 
 export default function App() {
     return (
         <>
-            <h1>Buscador de Clima</h1>
+            <h1 className={styles.title}>Buscador de Clima</h1>
+            <div className={styles.container}>
+                <p>1</p>
+                <p>2</p>
+            </div>
         </>
     )
 }
