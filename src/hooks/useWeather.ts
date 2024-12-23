6662d4adedd1196c94439b431dc28f03dddda531
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-23 18:10:47 -0300

    Segundo llamado hacia la API para obtener el clima

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -3,12 +3,19 @@ import type {SearchType} from "../types"
 
 export default function useWeather() {
     const fetchWeather = async(search: SearchType) => {
+        const appId = import.meta.env.VITE_API_KEY
         console.log('Consultando...')
         try {
-            const appId = import.meta.env.VITE_API_KEY
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+            
             const {data} = await axios(geoUrl)
-            console.log(data) 
+            const lat = data[0].lat
+            const lon = data[0].lon
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+            const {data: weatherResult} = await axios(weatherUrl)
+
+            console.log(weatherResult)
+            
             
                         
         } catch (error) {
