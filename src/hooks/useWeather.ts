bb8079e8da69437c90dc65d8230e0344fbef4fb1
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-23 17:58:57 -0300

    Variables de entorno para el API Key

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -5,7 +5,7 @@ export default function useWeather() {
     const fetchWeather = async(search: SearchType) => {
         console.log('Consultando...')
         try {
-            const appId = 'a3779f0c2090cf5e7942339f8d308cfd'
+            const appId = import.meta.env.VITE_API_KEY
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
             const {data} = await axios(geoUrl)
             console.log(data) 
