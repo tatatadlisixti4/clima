77f1df54d2495fc2b48f9085137b51c1cb9268aa
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-23 23:12:40 -0300

    Colocando el resultado de la API en el state

---
+++ src/components/WeatherDetail/WeatherDetail.tsx
@@ -0,0 +1,12 @@
+import { Weather } from "../../hooks/useWeather"
+
+
+type WeatherDetailProps = {
+    weather: Weather
+}
+
+export default function WeatherDetail({weather} : WeatherDetailProps) {
+    return (
+        <div>WeatherDetail</div>
+    )
+}
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,20 +1,28 @@
-import axios from "axios"
-import {object, string, number, Output, parse} from 'valibot'
+import {useState} from 'react'
 import type {SearchType} from "../types"
+import {z} from 'zod'
+import axios from "axios"
 
 // Valibot esquema
-const WeatherSchema = object({
-    name: string(),
-    main: object({
-        temp: number(),
-        temp_max: number(),
-        temp_min: number()
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_max: z.number(),
+        temp_min: z.number()
     })
 })
-
-type Weather = Output<typeof WeatherSchema>
+export type Weather = z.infer<typeof Weather> 
 
 export default function useWeather() {
+    const [weather, setWeather] = useState<Weather>({
+        name: '',
+        main: {
+            temp: 0,
+            temp_max: 0,
+            temp_min: 0
+        }
+    })
     const fetchWeather = async(search: SearchType) => {
         const appId = import.meta.env.VITE_API_KEY
         console.log('Consultando...')
@@ -23,17 +31,23 @@ export default function useWeather() {
             const {data} = await axios(geoUrl)
             const lat = data[0].lat
             const lon = data[0].lon
-
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
             const {data: weatherResult} = await axios(weatherUrl)
-            const result = parse(WeatherSchema,  weatherResult)
-            if(result) {
-                console.log(result)
+            const result = Weather.safeParse(weatherResult)
+            
+            if(result.success) {
+                setWeather(result.data)
+            } else {
+                console.log('Error al validar res.api con esquema zod');
             }
+            
         } catch (error) {
             console.log(error)
             
         }
     }
-    return {fetchWeather}
+    return {
+        weather,
+        fetchWeather
+    }
 }
